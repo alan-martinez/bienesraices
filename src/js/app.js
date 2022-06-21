56b55e4dd48d9caff284ecd6aab3e43e482fd19c
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-21 10:07:38 -0500

    Dark mode finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMode() {
+    //Leer preferencias del usuario
+    const prefiereDarkMode = window.matchMedia('{prefers-color-scheme: dark}');
+    
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    }else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        }else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMod = document.querySelector('.dark-mode-boton');
     
+    botonDarkMod.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+
+    });
 }
 
 function eventListeners() {
